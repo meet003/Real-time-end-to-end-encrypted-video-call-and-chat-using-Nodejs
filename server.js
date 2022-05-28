0e7c3d70d8ff86d36c99b334a1160df968c05140
meet savaliya <msavaliya023@gmail.com>
2022-05-28 21:19:50 +0530

    port

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+app.use('/peerjs', peerServer);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        console.log("joined room");
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit('user-connected', userId);
+        socket.on('message', (message) => {
+            //send message to the same room
+            io.to(roomId).emit('createMessage', message)
+        });
+    })
+})
+
+
+server.listen(process.env.PORT || 3000);
